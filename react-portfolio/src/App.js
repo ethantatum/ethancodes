ba8cc7d5e6e0f29643c4d93e90fdfa03e55c15e1
Ethan Tatum <ethantatum81@gmail.com>
2020-01-28 19:31:11 -0700

    added Home page component

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Portfolio from "./pages/Portfolio/Portfolio";
+import Home from "./pages/Home/Home";
 
 const App = () => {
   return (
     <Router>
       <Navbar />
       <Switch>
-        <Route path="/" component={Portfolio} />
+        <Route path="portfolio" component={Portfolio} />
+        <Route path="/" component={Home} />
       </Switch>
     </Router>
   );
